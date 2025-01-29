f12541bbeed2d61c45b719c0d1ba0e8f24447230
DESKTOP-BHESTOL\william <williamwolff05@gmail.com>
2025-01-29 19:47:06 +0100

    fix componet

--- my-vue-app/src/App.tsx
+++ my-vue-app/src/App.tsx
@@ -1,60 +1,27 @@
-import { useState } from 'react'
-import React from "react";
-import './App.css'
+import ModelTable from "./modelTable";
 
-interface Modeller {
-  Name: String
-  Model: string
-  Manufacturer: string
 
-}
 
-interface tableProp {
-  modeller: Modeller[];
-  rows: number;
-  columns: number;
+function App() {
   
-}
-
-const ModelTable: React.FC<tableProp> = ({ modeller }) => {
+  const modellerData = [
+    {Name: "CR90 corvette", Model: "CR90 corvette", Manufacturer: "Corellian Engineering Corporation"},
+    {Name: "Star Destroyer", Model: "Imperial I-class Star Destroyer", Manufacturer: "Kuat Drive Yards"},
+    {Name: "Millennium Falcon", Model: "YT-1300 light freighter", Manufacturer: "Corellian Engineering Corporation"},
+  ]
+  
+  
+  
   return (
-    <table style={{ border: "1px solid black", borderCollapse: "collapse" }}>
-      <thead>
-        <tr>
-          <th style={{ border: "1px solid black", padding: "8px" }}>Name</th>
-          <th style={{ border: "1px solid black", padding: "8px" }}>Model</th>
-          <th style={{ border: "1px solid black", padding: "8px" }}>Manufacturer</th>
-        </tr>
-      </thead>
-      <tbody>
-      {modeller.map((item, rowindex) => (
-          <tr key={rowindex}>
-            
-            <td  style={{
-                border: "1px solid black",
-                padding: "8px",
-                backgroundColor: "#ffcccc", // Highlight for Name column
-              }}>{item.Name}</td>
 
-            <td               style={{
-                border: "1px solid black",
-                padding: "8px",
-                backgroundColor: "#ccffcc", // Highlight for Model column
-              }}>{item.Model}</td>
-
-            <td style={{
-                border: "1px solid black",
-                padding: "8px",
-                backgroundColor: "#ccccff", // Highlight for Manufacturer column
-              }}>{item.Manufacturer}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  )
-};
-
-export default ModelTable
+    <div style={{ padding: "20px" }}>
+      <h1>Model Collection</h1>
+      <ModelTable modeller={modellerData}   />
+    </div>
 
 
+    
 
+    );
+}
+export default App;
--- my-vue-app/src/main.tsx
+++ my-vue-app/src/main.tsx
@@ -1,23 +1,9 @@
 import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom';
 import './index.css'
-import ModelTable from './App.tsx';
+import App from './App.tsx';
 
 
-const App: React.FC = () => {
-  const modellerData = [
-    {Name: "CR90 corvette", Model: "CR90 corvette", Manufacturer: "Corellian Engineering Corporation"},
-    {Name: "Star Destroyer", Model: "Imperial I-class Star Destroyer", Manufacturer: "Kuat Drive Yards"},
-    {Name: "Millennium Falcon", Model: "YT-1300 light freighter", Manufacturer: "Corellian Engineering Corporation"},
-  ]
-
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Model Collection</h1>
-      <ModelTable modeller={modellerData} rows={3} columns={3} />
-    </div>
-  );
-};
 
 ReactDOM.render(
   <React.StrictMode>
